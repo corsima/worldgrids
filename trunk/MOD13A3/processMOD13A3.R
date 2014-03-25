69bce8e555fb5add2203695c87c0fd1ada52333a
tom.hengl@gmail.com <tom.hengl@gmail.com@2ab5ac03-5a3f-1791-be6b-75812adde1f0>
2014-03-25 10:27:23 +0000

    updated derivation of EVI mean, sd high and low images

--- trunk/MOD13A3/processMOD13A3.R
+++ trunk/MOD13A3/processMOD13A3.R
@@ -34,14 +34,15 @@ for(j in 1:nrow(tiles)){
     tilename <- paste(outname, '_', j, '.sdat', sep="")
     # resample
     if(is.na(file.info(tilename)$size)){
-      system(paste(gdalwarp, ' ', dtif.lst[i], ' ', tilename, ' -of \"SAGA\" -dstnodata \"-3000\" -r near -te ', tiles[j,"lonmin"] , ' ', tiles[j,"latmin"], ' ', tiles[j,"lonmax"] ,' ', tiles[j,"latmax"] ,' -tr ', 1/120, ' ', 1/120, sep=""))
+      system(paste(gdalwarp, ' ', dtif.lst[i], ' ', tilename, ' -of \"SAGA\" -dstnodata \"-32767\" -r near -te ', tiles[j,"lonmin"] , ' ', tiles[j,"latmin"], ' ', tiles[j,"lonmax"] ,' ', tiles[j,"latmax"] ,' -tr ', 1/120, ' ', 1/120, sep=""))
     }
   }
   # derive derive mean, sd, min and max values:
   t.lst <- list.files(pattern=glob2rx(paste('EVI_1km_*_*_*_', j, '.sgrd$', sep="")))
   if(is.na(file.info(paste("EVMMOD3a_", j, ".sgrd", sep=""))$size)){
-  rsaga.geoprocessor(lib="geostatistics_grid", module=4, param=list(GRIDS=paste(t.lst, collapse=";"), MEAN=paste("EVMMOD3a_", j, ".sgrd", sep=""), MIN=paste("EVLMOD3a_", j, ".sgrd", sep=""), MAX=paste("EVHMOD3a_", j, ".sgrd", sep=""), STDDEV=paste("EVSMOD3a_", j, ".sgrd", sep="")))
-  # rsaga.geoprocessor(lib="geostatistics_grid", module=8, param=list(GRIDS=paste(t.lst, collapse=";"), PCA=paste("EV_MOD3a_", j, "_", sep=""), METHOD=1, NFIRST=3))
+     rsaga.geoprocessor(lib="geostatistics_grid", module=4, param=list(GRIDS=paste(t.lst, collapse=";"), MEAN=paste("EVMMOD3a_", j, ".sgrd", sep=""), MIN=paste("EVLMOD3a_", j, ".sgrd", sep=""), MAX=paste("EVHMOD3a_", j, ".sgrd", sep=""), STDDEV=paste("EVSMOD3a_", j, ".sgrd", sep="")))
+    #rsaga.geoprocessor(lib="geostatistics_grid", module=8, param=list(GRIDS=paste(t.lst, collapse=";"), PCA=paste("EV_MOD3a_", j, "_", sep=""), METHOD=1, NFIRST=3))
+    ## TH: this takes too much time...
   }
 }
 
@@ -49,20 +50,37 @@ for(j in 1:nrow(tiles)){
 for(j in c("EVMMOD", "EVSMOD", "EVLMOD", "EVHMOD")){
   unlink("mod13a3.vrt")
   system(paste(gdalbuildvrt, "mod13a3.vrt", paste(j, "3a_", 1:nrow(tiles), ".sdat", sep="", collapse=" ")))
-  # convert to geotif (1 km):
+  ## convert to geotif (1 km):
   if(is.na(file.info(paste(j, '3a.tif', sep=""))$size)){
-  system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -dstnodata \"-3000\" -ot Int16 ', j, '3a.tif', ' -r near -te -180 -90 180 90 -tr ', 1/120,' ', 1/120, sep=""))
+    if(j=="EVSMOD"){
+      system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 ', j, '3a.tif', ' -r near -te -180 -90 180 90 -tr ', 1/120,' ', 1/120, sep=""))
+    } else {
+      system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 -dstnodata \"-32767\" ', j, '3a.tif', ' -r near -te -180 -90 180 90 -tr ', 1/120,' ', 1/120, sep=""))   ##  
+    }
   }
   # 2.5 km:
   if(is.na(file.info(paste(j, '2a.tif', sep=""))$size)){
-  system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -dstnodata \"-3000\" -ot Int16 ', j, '2a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/40,' ', 1/40, sep=""))
+    if(j=="EVSMOD"){
+      system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 ', j, '2a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/40,' ', 1/40, sep=""))
+    } else {
+      system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 -dstnodata \"-32767\" ', j, '2a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/40,' ', 1/40, sep=""))
+    }
   }
   # 5 km:
   if(is.na(file.info(paste(j, '1a.tif', sep=""))$size)){
-  system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -dstnodata \"-3000\" -ot Int16 ', j, '1a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/20,' ', 1/20, sep=""))
+    if(j=="EVSMOD"){
+      system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 ', j, '1a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/20,' ', 1/20, sep=""))
+    } else {
+      system(paste(gdalwarp, ' mod13a3.vrt -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 -dstnodata \"-32767\" ', j, '1a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/20,' ', 1/20, sep=""))
+    }
   }
-  # 20 km:
-  system(paste(gdalwarp, ' ', j, '1a.tif', ' -t_srs \"+proj=longlat +datum=WGS84\" -dstnodata \"-3000\" -ot Int16 ', j, '0a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/5,' ', 1/5, sep=""))
+  ## 20 km:
+  ## TH: This creates a line around equator!! I had to re-make these maps by hand on the end in SAGA GIS. 
+  #if(j=="EVSMOD"){
+  #  system(paste(gdalwarp, ' ', j, '1a.tif', ' -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 ', j, '0a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/5,' ', 1/5, sep=""))
+  #} else {
+  #  system(paste(gdalwarp, ' ', j, '1a.tif', ' -t_srs \"+proj=longlat +datum=WGS84\" -ot Int16 -dstnodata \"-32767\" ', j, '0a.tif', ' -r bilinear -te -180 -90 180 90 -tr ', 1/5,' ', 1/5, sep=""))  
+  #}
 }
 
 ## compress:  
